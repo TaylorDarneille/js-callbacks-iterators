83d2855e50601fc392b7e01b532969628d6f3627
cam-em <cmoreno.1117@gmail.com>
2020-08-30 18:31:16 -0500

    completed reduce.js -saving

--- reduce.js
+++ reduce.js
@@ -2,9 +2,16 @@
 
 var numbers = [1, 56, 2, 4, 1, 99, 3, 5];
 
-// Your code here!
+
 
 // Write something that takes the above array and returns the product of all the numbers
 // via multiplication
 
 
+// Your code here!
+
+const multiplied = numbers.reduce((total, num) => {
+  return total * num;
+});
+
+console.log(multiplied);
