9fbaf6293d4aa6bc6e6f14bb390af3c7764ad72a
taylorschmidt <taylorhillschmidt@gmail.com>
2020-08-30 16:44:51 -0600

    added solutions to 7 files

--- map2.js
+++ map2.js
@@ -31,3 +31,11 @@ var phoneBook = [
 // For example, the 0th element of the new array should be {Abe: "1-111-111-1111"}
 
 //Your code here
+const newPhoneBook = phoneBook.map((element) => {
+  let name = Object.keys(element)
+  let number = element[name]
+  let newObject = {}
+  newObject[name] = number.charAt(0) + '-' + number;
+  return newObject
+})
+console.log(newPhoneBook)
