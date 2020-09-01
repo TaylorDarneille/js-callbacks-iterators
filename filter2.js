9606a8122127a6493c58f484bdd43f92343b0aa0
Gabbytam <gtam2@fordham.edu>
2020-08-31 20:34:21 -0700

    solution for filter2

--- filter2.js
+++ filter2.js
@@ -4,3 +4,9 @@ var misc = [3, "true", {a: 1, b: 2}, 7, [1, 2, 3], ['a', 'b', 'c'], "my favorite
 // Don't worry about strings in the nested array (misc[5] is considered an array, not a string)
 
 // Your code goes here
+let stringArr= misc.filter((string => {
+    //typeof (lowercase) operator used to test or find elements by their type
+    return !(typeof string === "string");
+  } ));
+  
+  console.log(stringArr);
